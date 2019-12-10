8f445ba1e30eee3ba025b706fd4012f5547eba87
202255 <202255@CARBON.CTI.P.LODZ.PL>
2019-12-10 18:40:18 +0100

    Added core functonality
    Added saving to .ics file and getting event information.

--- zad3/src/main/java/com/ppkwu/zad/CalendarController.java
+++ zad3/src/main/java/com/ppkwu/zad/CalendarController.java
@@ -1,12 +1,26 @@
 package com.ppkwu.zad;
 
+import biweekly.Biweekly;
 import biweekly.ICalendar;
+import biweekly.component.VEvent;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @RestController
@@ -32,5 +46,43 @@ public class CalendarController {
         ICalendar calendar = new ICalendar();
         LocalDate localDate = LocalDate.now();
         Month actualMonth = localDate.getMonth();
+
+        try {
+            Document HtmlDocument = Jsoup.connect(url).get();
+            for (Element tdElement : HtmlDocument.select(HTML_SELECTOR)) {
+                for (Element activeClassElement : tdElement.getElementsByClass(CLASS_NAME)) {
+                    if (activeClassElement.toString().contains(MATCHER)) {
+                        String[] splitted = activeClassElement.toString().split(REGEX);
+                        String day = splitted[INDEX].substring(splitted[INDEX].lastIndexOf("\">") + 2, splitted[INDEX].lastIndexOf("<"));
+                        for (Element pElement : activeClassElement.getElementsByTag("p")) {
+                            String pString = pElement.toString();
+                            String event = pString.substring(pString.indexOf(">") + 1, pString.lastIndexOf("<"));
+                            calendarEvents.add(new Calendar(day, event));
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (Calendar calendarEvent : calendarEvents) {
+            VEvent event = new VEvent();
+            event.setSummary(calendarEvent.getEventName());
+            Date eventDate = new GregorianCalendar(localDate.getYear(), actualMonth.getValue(), Integer.parseInt(calendarEvent.getDay()) + 1).getTime();
+            event.setDateStart(eventDate);
+            event.setDateEnd(eventDate);
+            calendar.addEvent(event);
+        }
+
+        File calendarFile = new File(FILENAME);
+        try {
+            Biweekly.write(calendar).go(calendarFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Resource fileSystemResource = new FileSystemResource(calendarFile);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(MEDIA_TYPE))
+                .body(fileSystemResource);
     }
 }
